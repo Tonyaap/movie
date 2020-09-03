f6c202a37516fb02b10eab21866e413a5630c4a6
Tonyaap <stefan_kniest_@hotmail.com>
2020-09-03 13:39:22 +0200

    First steps of the movie site

---
+++ src/components/NavBar.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Switch, Route, NavLink } from "react-router-dom";
+
+export default function navbar() {
+  return (
+    <div className="nav">
+      <NavLink
+        to="/"
+        exact="true"
+        activeStyle={{ fontWeight: "bold", color: "red" }}
+      >
+        {" "}
+        Home{" "}
+      </NavLink>
+      <NavLink to="/about" activeStyle={{ fontWeight: "bold", color: "red" }}>
+        {" "}
+        About{" "}
+      </NavLink>
+      <NavLink to="/users" activeStyle={{ fontWeight: "bold", color: "red" }}>
+        Users
+      </NavLink>
+    </div>
+  );
+}
